5b38806ed06ef604254a3859554fe5c2911ade0f
nicoleurs <leurs.nicolas96@gmail.com>
2023-09-20 14:14:47 +0200

    new customizable questions and answers, clear command for nicer interface

--- Quizz.py
+++ Quizz.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python3
 
-def quizz(questions:list[str],answers:list[str],chances:int):
+def quizz(questions:list[str],answers:list[str],guesses:int):
 
     user_answers = [None] * len(questions)
-    print("Welcome to my Quizz !!\nYou have 3 chances.")
+    print("Welcome to my Quizz !!\nYou have 3 guesses.\n")
     for i in range(len(questions)):   #### loop for questions in question to remove all [i]s
-        if chances > 0:
+        if guesses > 0:
             user_answers[i] = input(questions[i])
             while answers[i] != user_answers[i].lower():
-                    chances -= 1
-                    if chances > 0:
+                    guesses -= 1
+                    if guesses > 0:
                         print(f"Your answer : {user_answers[i]}")
-                        print('Too bad! That is not the correct answer')
-                        print(f"You have {chances} chances left")
+                        print('Too bad! That is not the correct answer\n')
+                        print(f"You have {guesses} guesses left")
                         user_answers[i] = input(questions[i])
                     else: 
                         print(f"Your answer : {user_answers[i]}")
-                        print("Sorry, you have 0 chances left")
+                        print("Sorry, you have 0 guesses left\n")
                         print("Oh no, you lost the quiz...")
                         break
             else: 
                 print(f"Your answer : {user_answers[i]}")
                 print('Good job! This is the right answer')
-    if chances > 0:
+    if guesses > 0:
         print("Congratulations, you win!")
- 
-q = ['Who is the best superhero?', "What rhymes with tomato?", "Cats or dogs?","Hello?"]
+        
+import os
+os.system('clear')
 
-a = ['batman', 'potato', 'dogs','world']
+print("First set up the quizz!\n")
 
-quizz(q, a, chances = 4)
+q = input("Enter your questions here! Please separate them by a comma: ")
+question = q.split(',')
+
+a = input("Enter your answers here! Please separate them by a comma: ")
+answer = a.split(',')
+
+guess = int(input("Enter number of guesses : "))
+
+os.system('clear')
+
+print("Great! Are your ready?\n")
+k = input("Press any key to continue\n")
+
+quizz(question, answer, guess)
