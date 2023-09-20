0aaf54b2b99ce979a4846b3bd04b47e7a7c5f513
nicoleurs <leurs.nicolas96@gmail.com>
2023-09-20 15:19:22 +0200

    prevent user from inputing different number of Q&A

--- Quizz.py
+++ Quizz.py
@@ -2,25 +2,23 @@
 
 def quizz(questions:list[str],answers:list[str],guesses:int):
 
-    user_answers = [None] * len(questions)
+    user_answers = None
     print("Welcome to my Quizz !!\nYou have 3 guesses.\n")
     for i in range(len(questions)):   #### loop for questions in question to remove all [i]s
         if guesses > 0:
-            user_answers[i] = input(questions[i])
-            while answers[i] != user_answers[i].lower():
+            user_answers = input(questions[i])
+            while answers[i] != user_answers.lower():
                     guesses -= 1
                     if guesses > 0:
-                        print(f"Your answer : {user_answers[i]}")
                         print('Too bad! That is not the correct answer\n')
                         print(f"You have {guesses} guesses left")
-                        user_answers[i] = input(questions[i])
+                        user_answers = input(questions[i])
                     else: 
-                        print(f"Your answer : {user_answers[i]}")
+                        print(f"Your answer: {user_answers}")
                         print("Sorry, you have 0 guesses left\n")
                         print("Oh no, you lost the quiz...")
                         break
             else: 
-                print(f"Your answer : {user_answers[i]}")
                 print('Good job! This is the right answer\n')
     if guesses > 0:
         print("Congratulations, you win!")
@@ -33,16 +31,18 @@ print("First set up the quizz!\n")
 q = input("Enter your questions here! Please separate them by a comma: ")
 question = q.split(',')
 question = [item.strip() for item in question]
+question = [item + " \n"for item in question]
 
 a = input("Enter your answers here! Please separate them by a comma: ")
 answer = a.split(',')
 answer = [item.strip() for item in answer]
 
-guess = int(input("Enter number of guesses : "))
+if len(question) != len(answer):
+    print("You must have as many answers as you have questions!")
+else:
+    guess = int(input("Enter number of guesses: "))
 
-os.system('clear')
-
-print("Great! Are your ready?\n")
-k = input("Press any key to continue\n")
-
-quizz(question, answer, guess)
+    os.system('clear')
+    print("Great! Are your ready?\n")
+    k = input("Press any key to continue\n")
+    quizz(question, answer, guess)
